3d77bd20c50d1c6df3e74e4acfecffb4f860c7b8
Boppr <crazy_boppr@live.com>
2021-04-09 15:35:14 +1000

    Initial commit

---
+++ MonkeyMod.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using MelonLoader;
+
+namespace MonkeyMod
+{
+    public class Class : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
